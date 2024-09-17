226d451c4440b719b3573799e0602c6433969bbf
Reda Salmi <reda.salmi.elt@gmail.com>
2024-09-17 21:10:21 +0100

    adds dummy navbar fetch

--- src/layouts/notes.tsx
+++ src/layouts/notes.tsx
@@ -1,17 +1,64 @@
 import { A } from "@solidjs/router";
-import type { ParentComponent } from "solid-js";
+import {
+	createResource,
+	For,
+	Match,
+	Suspense,
+	Switch,
+	type ParentComponent,
+} from "solid-js";
+
+const fetchNotes = async () => {
+	const notes = await new Promise((resolve) => {
+		setTimeout(() => {
+			resolve([
+				{
+					slug: "note-1",
+					title: "Note 1",
+				},
+				{
+					slug: "note-2",
+					title: "Note 2",
+				},
+				{
+					slug: "note-3",
+					title: "Note 3",
+				},
+				{
+					slug: "note-4",
+					title: "Note 4",
+				},
+			]);
+		}, 1000);
+	});
+
+	return notes;
+};
 
 export const NotesLayout: ParentComponent = (props) => {
+	const [notes] = createResource(fetchNotes);
+
 	return (
 		<div>
-			<nav>
-				<A href="/notes/note-1" activeClass="underline">
-					note 1
-				</A>
-				<A href="/notes/note-2" activeClass="underline">
-					note 2
-				</A>
-			</nav>
+			<Suspense>
+				<Switch>
+					<Match when={notes.error}>
+						<span>Error: {notes.error.message}</span>
+					</Match>
+					<Match when={notes()}>
+						<nav class="flex gap-2">
+							<For each={notes()}>
+								{(note) => (
+									<A href={`/notes/${note.slug}`} activeClass="underline">
+										{note.title}
+									</A>
+								)}
+							</For>
+						</nav>
+					</Match>
+				</Switch>
+			</Suspense>
+
 			{props.children}
 		</div>
 	);
